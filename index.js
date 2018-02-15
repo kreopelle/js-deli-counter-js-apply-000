25b623c87ebe36eff4e78eaeb6fe8726a85663f2
Kayla Reopelle <kreopelle@gmail.com>
2018-02-15 05:40:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function takeANumber(katzDeliLine, newName) {
+  var newArray = [];
+  for (var i = 0; i < katzDeliLine.length; i++) {
+    newArray.push(newName);
+  }
   return `Welcome, ${newName}. You are number ${katzDeliLine.length + 1} in line.`;
 }
