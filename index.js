829dca0503a0495fb75a2ef72a836878136beb59
Kayla Reopelle <kreopelle@gmail.com>
2018-02-15 05:50:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ function takeANumber(katzDeliLine, newName) {
   return `Welcome, ${newName}. You are number ${katzDeliLine.length} in line.`;  
 }
 
+function nowServing() {
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine[0].
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
